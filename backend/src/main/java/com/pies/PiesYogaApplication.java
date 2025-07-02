34e5a42dc298e5beff281fdd58158fa79231180e
jamesyeh-vt <jamesyeh@vt.edu>
2025-07-01 23:04:32 -0400

    add: github actions and ci/cd config

--- backend/src/main/java/com/pies/PiesYogaApplication.java
+++ backend/src/main/java/com/pies/PiesYogaApplication.java
@@ -2,12 +2,19 @@ package com.pies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class PiesYogaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PiesYogaApplication.class, args);
+        SpringApplication app = new SpringApplication(PiesYogaApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        String profile = String.join(",", env.getActiveProfiles());
+        System.out.println("\n=========================================");
+        System.out.println("     Spring Boot Active Profile: " + (profile.isEmpty() ? "default" : profile));
+        System.out.println("=========================================\n");
     }
+
 
 }
