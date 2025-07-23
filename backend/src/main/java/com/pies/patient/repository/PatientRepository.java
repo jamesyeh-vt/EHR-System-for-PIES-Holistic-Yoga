7897580a298aa94c43fab1c03bc1158e779a4b5a
Peyton Wiecking <pwiecking@outlook.com>
2025-07-23 13:03:52 -0400

    Update PatientRepository.java

--- backend/src/main/java/com/pies/patient/repository/PatientRepository.java
+++ backend/src/main/java/com/pies/patient/repository/PatientRepository.java
@@ -1,11 +1,12 @@
 package com.pies.patient.repository;
 
-import com.pies.patient.model.Patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import com.pies.patient.model.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
